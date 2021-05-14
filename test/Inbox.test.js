8de5a64380d1cee2e3238f482c246e22b5c18067
Sujata Regoti <sujata.regoti@gmail.com>
2021-05-14 22:04:11 +0530

    Add message read and update test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,10 +5,26 @@ const {interface, bytecode} = require('../compile');
 const web3 = new Web3(ganache.provider());
 
 describe('Inbox', () => {
-    it('deploy a contract', async () => {
+    let inbox = null, account = null;
+
+    before(async () => {
         const accounts = await web3.eth.getAccounts();
-        const inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, 
-            arguments: ["Hi there!"]}).send({from: accounts[0], gas: '1000000'});
-        console.log(inbox);    
+        account = accounts[0];
+        inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, 
+            arguments: ["Hi there!"]}).send({from: account, gas: '1000000'});
+    });
+    it('deploy a contract', async () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('has default message', async () => {
+        const actualMessage = await inbox.methods.message().call();
+        assert.strictEqual("Hi there!", actualMessage);
+    }); 
+
+    it('can update the message', async () => {
+        await inbox.methods.setMessage("bye").send({from: account});
+        const actualMessage = await inbox.methods.message().call();
+        assert.strictEqual("bye", actualMessage);
     });
 });
